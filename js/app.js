90e5ff03a8563a90cc58a808c648491aacb18c36
golib <native9898#gmail.com>
2020-01-12 15:44:56 +0500

    cashback-calculator

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-console.log('worked');
+const REGULAR_CATEGORY = 'regular';
+const INCREASED_CATEGORY = 'increased';
+const SPECIAL_CATEGORY = 'special';
+
+const purchases = [
+    {
+        amount: 1000,
+        category: REGULAR_CATEGORY,
+    },
+    {
+        amount: 10000,
+        category: INCREASED_CATEGORY,
+    },
+    {
+        amount: 1000,
+        category: REGULAR_CATEGORY,
+    },
+    {
+        amount: 1000,
+        category: SPECIAL_CATEGORY,
+    },
+];
+let cashback = 0;
+
+for (let i = 0; i < purchases.length; i++) {
+    const elem = purchases[i];
+    switch (elem.category) {
+        case REGULAR_CATEGORY:
+            cashback += (1 * elem.amount) / 100
+            break;
+        case INCREASED_CATEGORY:
+            cashback += (5 * elem.amount) / 100
+            break;
+        case SPECIAL_CATEGORY:
+            cashback += (30 * elem.amount) / 100
+        default:
+            break;
+
+    }
+}
+if (cashback > 3000)
+    cashback = 3000;
+    console.log(cashback);
